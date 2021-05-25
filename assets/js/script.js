2494ff034ab43bedd5806f24537d0190803d30d9
Kayla Dombrosky <kdombroskyy@live.com>
2021-05-25 00:23:23 -0400

    add drag/drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,6 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
-
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -189,6 +188,77 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+// to drag tasks within the same column and across other columns 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // loop over current set of children in sortable list
+    // array to store the task data in 
+    var tempArr = [];
+
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text() 
+        .trim();
+
+        var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+        // add task data to the temp array as an object
+        tempArr.push({
+          text: text,
+          date: date
+        });
+    });
+    console.log(tempArr);
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr; 
+    saveTasks();
+  }
+});
+
+// to drop tasks into trash
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
+// using moment.js and jQuery UI for dates
+//$("#modalDueDate").datepicker();
+
 // load tasks for the first time
 loadTasks();
-
